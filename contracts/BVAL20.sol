4c80733880d6012a34df758c90c592182bc3721c
Brandon Valosek <bvalosek@gmail.com>
2021-03-06 12:58:35 -0600

    add tests and cleanup BVAL20

--- contracts/BVAL20.sol
+++ contracts/BVAL20.sol
@@ -1,20 +1,26 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "./@openzeppelin/AccessControl.sol";
+import "./@openzeppelin/AccessControlEnumerable.sol";
 import "./@openzeppelin/ERC20.sol";
 
-contract BVAL20 is AccessControl, ERC20 {
+// a basic ERC20 coin with a deadmans switch and some RBAC functionality to keep
+// future iteration open while still allowing me progressively add more
+// delegated trust to the system
+contract BVAL20 is AccessControlEnumerable, ERC20 {
 
   uint private constant ONE_DAY =  60 * 60 * 24;
   uint private constant ONE_YEAR = ONE_DAY * 365;
   string private constant NAME = "@bvalosek Token";
   string private constant SYMBOL = "BVAL";
 
-  // able to mint $BVAL
+  // grants ability to mint $BVAL
   bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
 
-  // able to ping deadmans switch
+  // grants ability to move $BVAL w/o allowances
+  bytes32 public constant OPERATOR_ROLE = keccak256("OPERATOR_ROLE");
+
+  // grants able to ping deadmans switch
   bytes32 public constant DEADMAN_ROLE = keccak256("DEADMAN_ROLE");
 
   // timestamp after which minting is no longer possible
@@ -22,6 +28,7 @@ contract BVAL20 is AccessControl, ERC20 {
 
   constructor() ERC20(NAME, SYMBOL) {
     _deadmanTimestamp = block.timestamp + ONE_YEAR;
+
     _setupRole(DEFAULT_ADMIN_ROLE, _msgSender());
     _setupRole(MINTER_ROLE, _msgSender());
     _setupRole(DEADMAN_ROLE, _msgSender());
@@ -32,15 +39,37 @@ contract BVAL20 is AccessControl, ERC20 {
   // ---
 
   function mintTo(address account, uint256 amount) external {
-    require(stillAlive(), "deadmans switch has been tripped");
+    require(stillAlive(), "deadman's switch has been tripped");
     require(hasRole(MINTER_ROLE, _msgSender()), "requires MINTER_ROLE");
-    _mint(address(this), amount);
-    transfer(account, amount);
+    address self = address(this);
+
+    // mint tokens into the contract
+    _mint(self, amount);
+
+    // call transfer as "this" in order to transfer FROM the contract TO
+    // account. Calling w/o "this" would attempt to tranfer FROM msgSender
+    this.transfer(account, amount);
+  }
+
+  // ---
+  // ERC20 modifications
+  // ---
+
+  // patch transferFrom to allow JIT-allowance when msgSender has the OPERATOR
+  // role. this is a bit overreaching but allows for better UX and avoids having
+  // to make two trx for anything I build that manages $BVAL to start. this can
+  // be disabled in the future by removing all granted OPERATOR roles
+  function transferFrom(address sender, address recipient, uint256 amount) override public returns (bool) {
+    address msgSender = _msgSender();
+    if (hasRole(OPERATOR_ROLE, msgSender)) {
+      increaseAllowance(msgSender, amount);
+    }
+    return super.transferFrom(sender, recipient, amount);
   }
 
   // ---
   // Burnable implementation
-  //
+  // ---
 
   // burn msg sender's coins
   function burn (uint256 amount) external {
--- test/BVAL20.test.js
+++ test/BVAL20.test.js
@@ -2,37 +2,16 @@ const truffleAssert = require('truffle-assertions');
 const timeMachine = require('ganache-time-traveler');
 
 const BVAL20 = artifacts.require('BVAL20');
-const BVAL721 = artifacts.require('BVAL721');
 
 const factory = async (startDate = '2021-02-07') => {
   await timeMachine.advanceBlockAndSetTime(createTimestamp(startDate));
   return BVAL20.new();
 };
 
-const factory721 = () => BVAL721.new({ description: 'desc', data: 'data', baseURI: 'uri' });
-
-const TOKENS = [
-  // emission rate = 10, cost = 0
-  '0x01510000000a000000000000000960096001488848fe00010001000100010001',
-  // emission rate = 10, cost = 10
-  '0x0123000a000a000000000000000960096001488848fe00010001000100010001',
-  // emission rate = 10, cost = 0, mint date = 2025-02-28
-  '0x01ba0000000a00000000000000096009600148884eb300010001000100010001',
-];
-
-const mintNFT = async (instance, tokenId = TOKENS[0]) => {
-  await instance.startSequence('1', 'name', 'desc', 'image');
-  await instance.mint(tokenId, 'name', 'desc', 'data');
-  return tokenId;
-};
-
 const createTimestamp = (date) => Math.round(new Date(date).getTime() / 1000);
 
 // max gas for deployment
-const MAX_DEPLOYMENT_GAS = 1500000;
-
-// max amount of gas we want to allow for basic on-chain mutations
-const MAX_MUTATION_GAS = 100000;
+const MAX_DEPLOYMENT_GAS = 1800000;
 
 let snapshotId;
 beforeEach(async () => {
@@ -52,15 +31,6 @@ contract('BVAL20', (accounts) => {
       assert.isBelow(gasUsed, MAX_DEPLOYMENT_GAS);
       console.log('deployment', gasUsed);
     });
-    // it('should cost below target gas to claim', async () => {
-    //   const instance = await factory();
-    //   const instance721 = await factory721();
-    //   await instance.setContract(1, instance721.address);
-    //   const tokenId = await mintNFT(instance721);
-    //   await timeMachine.advanceBlockAndSetTime(createTimestamp('2021-03-01'));
-    //   const res = await instance.claim([tokenId]);
-    //   assert.isBelow(res.receipt.gasUsed, MAX_MUTATION_GAS);
-    // });
   });
   describe('metadata', () => {
     it('should return name', async () => {
@@ -74,151 +44,15 @@ contract('BVAL20', (accounts) => {
       assert.equal(symbol, 'BVAL');
     });
   });
-  // describe('setContract', () => {
-  //   it('should allow setting contracts', async () => {
-  //     const instance = await factory();
-  //     const instance721 = await factory721();
-  //     await instance.setContract(1, instance721.address);
-  //     const address = await instance.getContract(1);
-  //     assert.equal(address.toString(), instance721.address.toString());
-  //   });
-  //   it('should revert if same collection version set twice', async () => {
-  //     const instance = await factory();
-  //     const instance721 = await factory721();
-  //     await instance.setContract(1, instance721.address);
-  //     const task = instance.setContract(1, instance721.address);
-  //     await truffleAssert.fails(task, truffleAssert.ErrorType.REVERT, 'collection version already set');
-  //   });
-  // });
-  // describe('accumulation calculation', () => {
-  //   it('should accumulate based on date', async () => {
-  //     const instance = await factory();
-  //     const instance721 = await factory721();
-  //     const tokenId = await mintNFT(instance721);
-  //     await timeMachine.advanceBlockAndSetTime(createTimestamp('2021-03-01'));
-  //     const accumulated = await instance.accumulated(tokenId);
-  //     assert.equal(accumulated.toString(), '310000000000000000000'); // 10 + 300 bonus
-  //   });
-  //   it('should accumulate bonus right from the start', async () => {
-  //     const instance = await factory();
-  //     const instance721 = await factory721();
-  //     const tokenId = await mintNFT(instance721);
-  //     await timeMachine.advanceBlockAndSetTime(createTimestamp('2021-02-28'));
-  //     const accumulated = await instance.accumulated(tokenId);
-  //     assert.equal(accumulated.toString(), '300000000000000000000');
-  //   });
-  //   it('should accumulate 0 if mint date in the future', async () => {
-  //     const instance = await factory();
-  //     const instance721 = await factory721();
-  //     const tokenId = await mintNFT(instance721);
-  //     await timeMachine.advanceBlockAndSetTime(createTimestamp('2021-01-01'));
-  //     const accumulated = await instance.accumulated(tokenId);
-  //     assert.equal(accumulated.toString(), '0');
-  //   });
-  //   it('should not start with a bonus if minted after bonus date', async () => {
-  //     const instance = await factory();
-  //     const instance721 = await factory721();
-  //     const tokenId = await mintNFT(instance721, TOKENS[2]); // tokens mints on 2025-02-28
-  //     await timeMachine.advanceBlockAndSetTime(createTimestamp('2025-02-28'));
-  //     const accumulated = await instance.accumulated(tokenId);
-  //     assert.equal(accumulated.toString(), '0');
-  //   });
-  // });
-  // describe('claiming', () => {
-  //   it('should claim $BVAL', async () => {
-  //     const [a1] = accounts;
-  //     const instance = await factory();
-  //     const instance721 = await factory721();
-  //     await instance.setContract(1, instance721.address);
-  //     const tokenId = await mintNFT(instance721);
-
-  //     await timeMachine.advanceBlockAndSetTime(createTimestamp('2021-03-01'));
-  //     await instance.claim([tokenId]);
-  //     const balance = await instance.balanceOf(a1);
-  //     assert.equal(balance.toString(), '310000000000000000000'); // 10 + 300 bonus
-
-  //     // ensure zero-ed out
-  //     const toClaim = await instance.accumulated(tokenId);
-  //     assert.equal(toClaim.toString(), '0');
-  //   });
-  //   it('should revert if nothing to claim', async () => {
-  //     const instance = await factory();
-  //     const instance721 = await factory721();
-  //     await instance.setContract(1, instance721.address);
-  //     const tokenId = await mintNFT(instance721);
-
-  //     await timeMachine.advanceBlockAndSetTime(createTimestamp('2020-05-01'));
-  //     const task = instance.claim([tokenId]);
-  //     await truffleAssert.fails(task, truffleAssert.ErrorType.REVERT, 'nothing to claim');
-  //   });
-  //   it('should revert if provided a non-valid token', async () => {
-  //     const instance = await factory();
-  //     const instance721 = await factory721();
-  //     await instance.setContract(1, instance721.address);
-  //     const task = instance.claim([0]);
-  //     await truffleAssert.fails(task, truffleAssert.ErrorType.REVERT, 'malformed token');
-  //   });
-  //   it('should revert if non-holder attempts to claim', async () => {
-  //     const [, a2] = accounts;
-  //     const instance = await factory();
-  //     const instance721 = await factory721();
-  //     await instance.setContract(1, instance721.address);
-  //     const tokenId = await mintNFT(instance721);
-
-  //     await timeMachine.advanceBlockAndSetTime(createTimestamp('2021-05-01'));
-  //     const task = instance.claim([tokenId], { from: a2 });
-  //     await truffleAssert.fails(task, truffleAssert.ErrorType.REVERT, 'only token holder can claim');
-  //   });
-  //   it('should not double claim if same tokenId used multiple times', async () => {
-  //     const [a1] = accounts;
-  //     const instance = await factory();
-  //     const instance721 = await factory721();
-  //     await instance.setContract(1, instance721.address);
-  //     const tokenId = await mintNFT(instance721);
-
-  //     await timeMachine.advanceBlockAndSetTime(createTimestamp('2021-03-01'));
-  //     await instance.claim([tokenId, tokenId, tokenId, tokenId, tokenId]);
-  //     const balance = await instance.balanceOf(a1);
-  //     assert.equal(balance.toString(), '310000000000000000000'); // 10 + 300
-  //   });
-  //   it('should only claim newly emitted coins', async () => {
-  //     const [a1] = accounts;
-  //     const instance = await factory();
-  //     const instance721 = await factory721();
-  //     await instance.setContract(1, instance721.address);
-  //     const tokenId = await mintNFT(instance721);
-
-  //     await timeMachine.advanceBlockAndSetTime(createTimestamp('2021-03-01'));
-  //     await instance.claim([tokenId]);
-
-  //     await timeMachine.advanceBlockAndSetTime(createTimestamp('2021-03-02'));
-  //     await instance.claim([tokenId]);
-
-  //     await timeMachine.advanceBlockAndSetTime(createTimestamp('2021-03-03'));
-  //     await instance.claim([tokenId]);
-
-  //     const balance = await instance.balanceOf(a1);
-  //     assert.equal(balance.toString(), '330000000000000000000'); // 10 + 10 + 10 + 300 bonus
-  //   });
-  // });
   describe('deadmans switch', () => {
-    // it('should not emit any more coins after deadman switch tripped', async () => {
-    //   const [a1] = accounts;
-    //   const instance = await factory('2021-02-28'); // start on mint date
-    //   const instance721 = await factory721();
-    //   await instance.setContract(1, instance721.address);
-    //   const tokenId = await mintNFT(instance721);
-
-    //   await timeMachine.advanceBlockAndSetTime(createTimestamp('2025-03-01'));
-    //   await instance.claim([tokenId]);
-    //   const balance = await instance.balanceOf(a1);
-    //   assert.equal(balance.toString(), '3950000000000000000000'); // 365*10 + 30 bonus
-
-    //   // ensure zero-ed out, even after advancing the clock
-    //   await timeMachine.advanceBlockAndSetTime(createTimestamp('2030-03-01'));
-    //   const toClaim = await instance.accumulated(tokenId);
-    //   assert.equal(toClaim.toString(), '0');
-    // });
+    it('should not allow minting after deadmans switch is tripped', async () => {
+      await timeMachine.advanceBlockAndSetTime(createTimestamp('2021-02-28'));
+      const instance = await factory();
+      await instance.mintTo(instance.address, 10000); // doesnt revert
+      await timeMachine.advanceBlockAndSetTime(createTimestamp('2023-02-28'));
+      const task = instance.mintTo(instance.address, 10000); // doesnt revert
+      await truffleAssert.fails(task, truffleAssert.ErrorType.REVERT, 'has been tripped');
+    });
     it('should not allow resetting timer after switch has tripped', async () => {
       await timeMachine.advanceBlockAndSetTime(createTimestamp('2021-02-28'));
       const instance = await factory();
@@ -228,34 +62,4 @@ contract('BVAL20', (accounts) => {
       await truffleAssert.fails(task, truffleAssert.ErrorType.REVERT, 'has been tripped');
     });
   });
-  // describe('burning on state change', () => {
-  //   it('should burn coins on state change', async () => {
-  //     const [a1] = accounts;
-  //     const instance = await factory();
-  //     const instance721 = await factory721();
-  //     await instance721.setCoinContract(instance.address);
-  //     const tokenId = await mintNFT(instance721, TOKENS[1]);
-
-  //     await timeMachine.advanceBlockAndSetTime(createTimestamp('2021-03-01'));
-  //     await instance.claim([tokenId]);
-
-  //     await instance721.setTokenState(tokenId, '123456', 0);
-  //     const balance = await instance.balanceOf(a1);
-  //     assert.equal(balance.toString(), '300000000000000000000'); // 10 + 300 - 10
-  //   });
-  //   it('should allow additional bribe during state change', async () => {
-  //     const [a1] = accounts;
-  //     const instance = await factory();
-  //     const instance721 = await factory721();
-  //     await instance721.setCoinContract(instance.address);
-  //     const tokenId = await mintNFT(instance721, TOKENS[0]);
-
-  //     await timeMachine.advanceBlockAndSetTime(createTimestamp('2021-03-01'));
-  //     await instance.claim([tokenId]);
-
-  //     await instance721.setTokenState(tokenId, '123456', '200000000000000000000');
-  //     const balance = await instance.balanceOf(a1);
-  //     assert.equal(balance.toString(), '110000000000000000000'); // 10 + 300 - 200
-  //   });
-  // });
 });
